c0570a8b6eba75fe6a52dca527008420273c63e2
Ben Robinson <brobinson@5amsolutions.com>
2017-04-28 17:55:24 -0400

    adding js docs

--- src/main/scripts/app/models/user.ts
+++ src/main/scripts/app/models/user.ts
@@ -1,8 +1,14 @@
 export class User {
+
     private emailAddress: string;
+
     constructor(emailAddr: string) {
         this.emailAddress = emailAddr;
     }
+
+    /**
+     * Get the email address of the user
+     */
     public getEmailAddr(): string {
         return this.emailAddress;
     }
--- src/main/scripts/app/services/userService.ts
+++ src/main/scripts/app/services/userService.ts
@@ -9,11 +9,18 @@ export class UserService {
         this.addUser('bmrobin9823@gmail.com');
     }
 
+    /**
+     * Add a user to the mail subscriber list
+     * @param emailAddr email address of the user to add
+     */
     public addUser(emailAddr: string) {
         console.log('adding user ' + emailAddr);
         this.userList.push(new User(emailAddr));
     }
 
+    /**
+     * Retrieve all users from the mail subscriber list
+     */
     public getUserList(): User[] {
         return this.userList;
     }
